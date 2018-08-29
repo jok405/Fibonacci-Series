95598e36334041d45743f467fb8766007ec5278d
jok405 <john.y.kim84@gmail.com>
2018-08-29 13:57:35 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+    public class Runner
+    {
+        public void Run()
+        {
+            Console.WriteLine(Fibonacci(6)); // 1,1,2,3,5,8,13
+        }
+
+        public int Fibonacci(int x)
+        {
+            if (x < 0) throw new ArgumentException(
+               "must be at least 0", nameof(x));
+            return Fib(x).current;
+
+            (int current, int previous) Fib(int i)
+            {
+                if (i == 0) return (1, 0);
+                var (current, previous) = Fib(i - 1);
+                return (current + previous, current);
+            }
+        }
+    }
+}
